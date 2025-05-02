2bd49b13f41a9ebe439d0cf2062c4f78d81b5d61
Nourhan Farag <137570718+NourhanFarag@users.noreply.github.com>
2025-05-02 07:55:02 +0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+//import 'firebase_options.dart';
 
 // Constants
 import 'constants/app_routes.dart';
@@ -29,7 +29,7 @@ Future<void> main() async {
     WidgetsFlutterBinding.ensureInitialized();
 
     await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
+     // options: DefaultFirebaseOptions.currentPlatform,
     );
 
     SystemChrome.setSystemUIOverlayStyle(
